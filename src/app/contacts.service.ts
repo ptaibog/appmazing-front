8443cbd47476eae44d9e2ff8ff518b87c198bca0
ptaibo <ptaibog@gmail.com>
2023-11-07 13:27:11 +0100

    Add Contact Service ts

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getContacts(): Observable<any> { 
+    const url = 'http://localhost:30030/contacts/getAll';
+    const headers = new HttpHeaders();
+    return this.http.get<any>(url, {headers});
+  }
 }
