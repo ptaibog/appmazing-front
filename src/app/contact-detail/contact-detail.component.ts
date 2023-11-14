8efcdb4b0befebb08b46bce3f73ad505100aa4f6
ptaibo <ptaibog@gmail.com>
2023-11-14 11:40:02 +0100

    Button edit (contact-detail)

--- src/app/contact-detail/contact-detail.component.ts
+++ src/app/contact-detail/contact-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../contacts.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-detail',
@@ -10,12 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 export class ContactDetailComponent implements OnInit {
   contact: any;
 
-  constructor(private contactsService: ContactsService, private route: ActivatedRoute) { }
+  constructor(private contactsService: ContactsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.contactsService.getContact(this.route.snapshot.params['id']).subscribe(data =>{
       this.contact = data;
     });
+  }
+
+  editContact(){
+    this.router.navigate(['/contact/edit', this.route.snapshot.params['id']]);
   }
 
 }
